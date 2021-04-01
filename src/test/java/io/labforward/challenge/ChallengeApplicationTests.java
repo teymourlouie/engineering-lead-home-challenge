db8a594597cbdfe6d30844491221d3bb60ae7b6e
Mehdi Teymourlouie <mehdi.teymourlouie@gmail.com>
2021-04-01 08:25:20 +0430

    init commit

---
+++ src/test/java/io/labforward/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.labforward.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
